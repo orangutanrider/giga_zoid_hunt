b0f7884729a495be5404b6885dfddfc754fc8840
Dominic Rooney <orangutanrider@gmail.com>
2023-10-02 20:39:17 +0100

    Box selection
    It debug logs the colliders you drag select over.

--- movement_selection_prototype_bevy_proj/src/player_controller/mouse_controls.rs
+++ movement_selection_prototype_bevy_proj/src/player_controller/mouse_controls.rs
@@ -1,6 +1,11 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::primitives::Aabb};
 use bevy_rapier2d::prelude::*;
 use mouse_tracking::MousePosWorld;
+
+#[derive(Component)]
+pub struct BoxSelectionEmpty{
+    pub origin: Vec2,
+}
 
 pub struct InitializePlugin;
 impl Plugin for InitializePlugin {
@@ -8,16 +13,96 @@ impl Plugin for InitializePlugin {
         println!("");
         println!("Initializing mouse_controls.rs");
         app
-           .add_systems(Update, update);
+            .add_systems(Startup, startup)
+            .add_systems(Update, (
+                update,
+                //selection_single_click,
+            )
+        );
     }
+}
+
+fn startup(mut commands: Commands){
+    commands.spawn(BoxSelectionEmpty{origin: Vec2::ZERO});
 }
 
 fn update(
     rapier_context: Res<RapierContext>,
     mouse_world: Res<MousePosWorld>,
     buttons: Res<Input<MouseButton>>,
-){
-    selection_single_click(rapier_context, mouse_world, buttons);
+    mut box_empty_q: Query<&mut BoxSelectionEmpty>,
+) {
+    if buttons.just_pressed(MouseButton::Left) {
+        box_empty_q.single_mut().origin = mouse_world.truncate();
+        return;
+    }
+
+    if buttons.just_released(MouseButton::Left) {
+
+        let (min, max) = get_min_max(box_empty_q.single().origin, mouse_world.truncate());
+
+        let mut intersections = false;
+        let mut num_selected = 0;
+        aabb_intersections(rapier_context, min, max, |entity|{
+            num_selected += 1;
+            intersections = true;
+            println!("E");
+            true
+        });
+        if intersections == true {
+            println!("num_selected: {}", num_selected);
+        }
+
+        return;
+    }
+}
+
+fn aabb_intersections(
+    rapier_context: Res<RapierContext>,
+    aabb_min: Vec2,
+    aabb_max: Vec2,
+    callback: impl FnMut(Entity) -> bool, // Callback called for each intersecting aabb collider
+) {
+    rapier_context.colliders_with_aabb_intersecting_aabb(
+        Aabb::from_min_max(Vec3::new(aabb_min.x, aabb_min.y, 0.0), Vec3::new(aabb_max.x, aabb_max.y, 0.0)), 
+        callback
+    );
+}
+
+fn get_min_max(vec1: Vec2, vec2: Vec2) -> (Vec2, Vec2) {
+    // There is probably a math way to do this, dunno what it is though
+
+    let mut max = Vec2::ZERO;
+    // max X
+    if vec1.x > vec2.x{
+        max.x = vec1.x;
+    } else{
+        max.x = vec2.x;
+    }
+    // max y
+    if vec1.y > vec2.y{
+        max.y = vec1.y;
+    }
+    else{
+        max.y = vec2.y;
+    }
+
+    let mut min = Vec2::ZERO;
+    // min X
+    if vec1.x < vec2.x{
+        min.x = vec1.x;
+    } else{
+        min.x = vec2.x;
+    }
+    // min y
+    if vec1.y < vec2.y{
+        min.y = vec1.y;
+    }
+    else{
+        min.y = vec2.y;
+    }
+    
+    (min, max)
 }
 
 // once box selection is added, there needs to be a thing for deciding whether to do this or box selection.
