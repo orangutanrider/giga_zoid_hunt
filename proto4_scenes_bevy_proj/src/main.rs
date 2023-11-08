29e73915a362747fda0953c80603618f977017dd
Dominic Rooney <orangutanrider@gmail.com>
2023-11-08 09:50:22 +0000

    Update main.rs

--- proto4_scenes_bevy_proj/src/main.rs
+++ proto4_scenes_bevy_proj/src/main.rs
@@ -1,3 +1,50 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+use mouse_tracking::prelude::*;
+use bevy::DefaultPlugins;
+use mouse_tracking::prelude::MousePosPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, bevy.");
+
+    let mut app = App::new();
+    app.add_plugins((
+        DefaultPlugins,
+        RapierPhysicsPlugin::<()>::default(),
+        MousePosPlugin,
+        InitializePlugin,
+        )
+    );
+
+    #[cfg(debug_assertions)]
+    app.add_plugins(
+        RapierDebugRenderPlugin{
+            mode: DebugRenderMode::all(),
+            ..default()
+        }
+    );
+
+    app.run();
+}
+
+pub struct InitializePlugin;
+impl Plugin for InitializePlugin {
+    fn build(&self, app: &mut App) {
+        println!("Initializing main");
+        app
+           .add_plugins((
+            ))
+           .add_systems(Startup, spawn_main_camera);
+    }
+}
+
+#[derive(Component)]
+pub struct MainCamera;
+fn spawn_main_camera(mut commands: Commands) {
+    println!("(Startup) main::spawn_main_camera");
+    commands
+       .spawn((MainCamera, Camera2dBundle::default()))
+       .add(InitMouseTracking)
+       .add(InitWorldTracking)
+       .insert(mouse_tracking::MainCamera);
 }
