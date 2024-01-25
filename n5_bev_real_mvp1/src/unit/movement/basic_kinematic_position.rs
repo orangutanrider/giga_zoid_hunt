709875f705d8533c6fb905c90bfb4aa3a2d26da2
Dominic Rooney <orangutanrider@gmail.com>
2024-01-25 12:06:43 +0000

    Mover

--- n5_bev_real_mvp1/src/unit/movement/basic_kinematic_position.rs
+++ n5_bev_real_mvp1/src/unit/movement/basic_kinematic_position.rs
@@ -1,8 +1,34 @@
-use bevy::prelude::*;
+use bevy:: prelude::*;
+use super::{BasicMover, KinematicPositionBasicMoverAugment};
 
 pub struct InitializePlugin;
 impl Plugin for InitializePlugin {
     fn build(&self, app: &mut App) {
-
+        app.add_systems(Update, move_update);
     }
+}
+
+fn move_update(
+    mover_q: Query<&BasicMover>,
+    mut aug_q: Query<&mut KinematicPositionBasicMoverAugment>,
+    mut transform_q: Query<&mut Transform>,
+) {
+    for aug in aug_q.iter_mut() {
+        movement(&mover_q, &mut transform_q, aug);
+    }
+}
+
+/// https://rapier.rs/docs/user_guides/bevy_plugin/rigid_bodies
+fn movement(
+    mover_q: & Query<&BasicMover>,
+    transform_q: &mut Query<&mut Transform>,
+    aug: Mut<'_, KinematicPositionBasicMoverAugment>,
+) {
+    let entity = aug.entity;
+    let transform = transform_q.get_mut(entity);
+    let mut transform = transform.unwrap();
+    let mover = mover_q.get(entity);
+    let mover = mover.unwrap();
+
+    transform.translation = transform.translation + mover.read_move_vec().extend(0.0);
 }
