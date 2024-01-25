c7af7dc90e0f40230a3d71f6e8c4a3bb2267d1ea
Dominic Rooney <orangutanrider@gmail.com>
2024-01-25 12:06:43 +0000

    Mover

--- n5_bev_real_mvp1/src/unit/movement.rs
+++ n5_bev_real_mvp1/src/unit/movement.rs
@@ -7,28 +7,41 @@ use bevy::prelude::*;
 
 // move input is edited directly
 #[derive(Component)]
-struct BasicMover{
+pub struct BasicMover{
     /// to be updated constantly, to direct the movement
-    pub move_input: Vec2, 
+    move_input: Vec2, 
     /// parameter, should be set on creation and never updated again
-    pub move_power: f32, 
+    move_power: f32, 
 }
+impl BasicMover{
+    pub fn new(move_power: f32) -> BasicMover {
+        return BasicMover { move_input: Vec2::ZERO, move_power };
+    }
 
-// This is un-implmented, but for this one the plan is that I'd have it recieve its move input data via functions
-// And then it could ping an event whenever the input was updated
-// This'd also mean that it'd have to have a read function, which would probably mean it'd have to copy the data 
-// It would be a better implmentation, but I don't need the functionality
-#[derive(Component)]
-struct AdvancedMover{
-    move_input: Vec2,
-    move_power: f32,
+    pub fn read_move_vec(&self) -> Vec2 {
+        return self.move_input * self.move_power;
+    }
+
+    pub fn input_move_vec(&mut self, move_vec: Vec2) {
+        self.move_input = move_vec.normalize() * move_vec.length().clamp(0.0, 1.0);
+    }
 }
 
 // Mover augments create the movement, by utlising movers
 #[derive(Component)]
-struct KinematicPositionBasicMoverAugment;
+pub struct KinematicPositionBasicMoverAugment{
+    entity: Entity,
+}
 impl KinematicPositionBasicMoverAugment {
     /// Intention is for this value to be used in the movement power calculation as a multiply on the final value.
     /// That way all movers of this type can be adjusted globally, while retaining their own movement power values
     pub const AUG_GLOBAL_POWER: f32 = 1.0; 
+
+    pub fn new(entity: Entity) -> KinematicPositionBasicMoverAugment {
+        return {
+            KinematicPositionBasicMoverAugment{
+                entity,
+            }
+        }
+    }
 }
