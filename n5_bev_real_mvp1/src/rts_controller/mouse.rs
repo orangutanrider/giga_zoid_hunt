326711e37917b83021c0f5af6ba44c1595af0b23
Dominic Rooney <orangutanrider@gmail.com>
2024-01-30 14:00:22 +0000

    rts controller refactor
    it is very clean :)

--- n5_bev_real_mvp1/src/rts_controller/mouse.rs
+++ n5_bev_real_mvp1/src/rts_controller/mouse.rs
@@ -2,42 +2,12 @@ use bevy::prelude::*;
 use bevy::ecs::system::SystemParam;
 use mouse_tracking::MousePosWorld;
 
-use super::unit_selection::SelectInput;
-
-pub struct InitializePlugin;
-impl Plugin for InitializePlugin {
-    fn build(&self, app: &mut App) {
-        app.init_resource::<SelectBoxOrigin>();
-        app.add_systems(Update, record_select_just_pressed_origin);
-    }
-}
-
 #[derive(SystemParam)]
 pub struct RtsMouse<'w> {
-    origin: Res<'w, SelectBoxOrigin>,
     position: Res<'w, MousePosWorld>,
 }
 impl<'w> RtsMouse<'w> {
     pub fn position(&self) -> Vec2 {
         return self.position.truncate();
     }
-
-    pub fn select_box_origin(&self) -> Vec2 {
-        return self.origin.0.truncate();
-    }
-}
-
-#[derive(Resource, Default)]
-/// Records and stores the mouse world position on SelectInput.just_pressed()
-struct SelectBoxOrigin(Vec3); 
-
-fn record_select_just_pressed_origin(
-    select_input: SelectInput,
-    mouse_world: Res<MousePosWorld>,
-    mut origin_keeper: ResMut<SelectBoxOrigin>,
-) {
-    if !select_input.just_pressed() {
-        return;
-    }
-    origin_keeper.0 = **mouse_world;
 }
--- n5_bev_real_mvp1/src/rts_controller/rapier_queries.rs
+++ n5_bev_real_mvp1/src/rts_controller/rapier_queries.rs
@@ -2,20 +2,25 @@ use bevy::prelude::*;
 use bevy::ecs::system::SystemParam;
 use bevy_rapier2d::prelude::*;
 
-use crate::rapier_config::prelude::E_ATTACKABLE_FILTER;
+use crate::rapier_config::prelude::{
+    E_ATTACKABLE_FILTER,
+    P_SELECTABLE_FILTER,
+};
 
 #[derive(SystemParam)]
-pub struct RtsCommanderRapierQueries<'w, 's>{
+pub struct RtsControllerRapierQueries<'w, 's>{
     rapier: Res<'w, RapierContext>,
 }
 
 /// Methods
-impl<'w, 's> RtsCommanderRapierQueries<'w, 's> {
+impl<'w, 's> RtsControllerRapierQueries<'w, 's> {
+    /// e_attackable = an attackable enemy
     pub fn cast_for_e_attackable(
         &self,
         location: Vec2,
     ) -> Option<(Entity, Toi)> {
         const SHAPE: Collider = Collider::ball(10.0);
+
         let rapier = self.rapier;
         return rapier.cast_shape(
             location, 
@@ -24,6 +29,29 @@ impl<'w, 's> RtsCommanderRapierQueries<'w, 's> {
             &SHAPE, 
             0.0,
             E_ATTACKABLE_FILTER,
+        )
+    }
+
+    /// p_selectable = a selectable unit on the player team
+    pub fn cast_for_p_selectable(
+        &self,
+        origin: Vec2,
+        release: Vec2,
+        callback: impl FnMut(Entity) -> bool, // called for each detected collider
+    ) {
+        let half_extents = (origin - release).abs(); // Dimensions
+        let half_extents = half_extents * 0.5; // Half extents
+        let shape = Collider::cuboid(half_extents.x, half_extents.y);
+        
+        let location = (origin + release) * 0.5;
+
+        let rapier = self.rapier;
+        return rapier.intersections_with_shape( // Only detects shapes that share its shape
+            location, 
+            0.0, 
+            &shape, 
+            P_SELECTABLE_FILTER, 
+            callback,
         )
     }
 }
