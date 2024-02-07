c54950bd37ef6715004c353e8a33cd7060858600
Dominic Rooney <orangutanrider@gmail.com>
2024-02-07 13:55:29 +0000

    detectors detached from behaviour entity
    Some units may have multiple detectors.
    For example, an autonomous unit might use a detector to decide if it needs to run away from an enemy.
    I will say though, doing this has made me consider the fact that maybe If I had just used sensor colliders for detection instead of ray-casters, maybe some of this work would've already have been done for me.
    In Unity, ray-casting was always my go to, it was much quicker and more simple than doing trigger collider stuff, maybe that isn't the case here though.

--- n5_bev_real_mvp1/src/rts_unit/behaviour/navigation/controlled/basic_controlled_navigation.rs
+++ n5_bev_real_mvp1/src/rts_unit/behaviour/navigation/controlled/basic_controlled_navigation.rs
@@ -5,6 +5,10 @@ use crate::rts_unit::behaviour::detection::single_result_types::{
     arbitrary_unit::ArbitraryUnitDetection,
     target_unit::TargetUnitDetection,
 };
+use crate::rts_unit::behaviour::detection::to_detection::attack_detection::{
+    ToAttackArbitraryDetection,
+    ToAttackTargetDetection,
+};
 use crate::rts_unit::{
     movement::Mover,
     ToRTSUnitRoot
@@ -15,7 +19,8 @@ pub struct InitializePlugin;
 impl Plugin for InitializePlugin{
     fn build(&self, app: &mut App) {
         app.add_systems(Update, (
-            behaviour_update,
+            behaviour_update_arb,
+            behaviour_update_target,
         ));
     }
 }
@@ -23,18 +28,19 @@ impl Plugin for InitializePlugin{
 #[derive(Component)]
 pub struct BasicControlled;
 
-fn behaviour_update (
-    behaviour_q: Query<(&ToRTSUnitRoot, &ArbitraryUnitDetection, &TargetUnitDetection), With<BasicControlled>>,
+fn behaviour_update_arb (
+    behaviour_q: Query<(&ToRTSUnitRoot, &ToAttackArbitraryDetection), With<BasicControlled>>,
+    detector_q: Query<&ArbitraryUnitDetection>,
     control_q: Query<&Commandable>,
     mut root_q: Query<(&mut Mover, &RTSUnitControlEntity, &Transform)>,
-
-    transform_q: Query<&Transform>,
 ) {
-    for (to_root, arb_detect, target_detect) in behaviour_q.iter() {
+    for (to_root, to_arb_detect) in behaviour_q.iter() {
         let root = to_root.root();
         let (mut mover, control_entity, transform) = root_q.get_mut(root.0).unwrap();
-        let commandable = control_q.get(control_entity.entity()).unwrap();
         let position = transform.translation.truncate();
+        let commandable = control_q.get(control_entity.entity()).unwrap();
+        let arb_detect = to_arb_detect.entity();
+        let arb_detect = detector_q.get(arb_detect).unwrap();
 
         let order = commandable.current_order();
         if order.is_none() {
@@ -44,6 +50,7 @@ fn behaviour_update (
         let order = order.unwrap();
 
         match order.order_type {
+            OrderType::AttackTarget(_) => { },
             OrderType::Empty => {
                 mover.input(Vec2::ZERO);
             },
@@ -53,6 +60,37 @@ fn behaviour_update (
             OrderType::AttackMove(attack_move_order) => {
                 follow_attack_move_order(arb_detect, mover, attack_move_order, position);
             },
+        }
+    }
+}
+
+fn behaviour_update_target (
+    behaviour_q: Query<(&ToRTSUnitRoot, &ToAttackTargetDetection), With<BasicControlled>>,
+    detector_q: Query<&TargetUnitDetection>,
+    control_q: Query<&Commandable>,
+    mut root_q: Query<(&mut Mover, &RTSUnitControlEntity, &Transform)>,
+
+    transform_q: Query<&Transform>,
+) {
+    for (to_root, to_target_detect) in behaviour_q.iter() {
+        let root = to_root.root();
+        let (mut mover, control_entity, transform) = root_q.get_mut(root.0).unwrap();
+        let position = transform.translation.truncate();
+        let commandable = control_q.get(control_entity.entity()).unwrap();
+        let target_detect = to_target_detect.entity();
+        let target_detect = detector_q.get(target_detect).unwrap();
+
+        let order = commandable.current_order();
+        if order.is_none() {
+            mover.input(Vec2::ZERO); 
+            return;
+        }
+        let order = order.unwrap();
+
+        match order.order_type {
+            OrderType::Empty => { },
+            OrderType::PureMovement(_) => { },
+            OrderType::AttackMove(_) => { },
             OrderType::AttackTarget(attack_target_order) => {
                 follow_attack_target_order(&transform_q, target_detect, mover, attack_target_order, position);
             },
