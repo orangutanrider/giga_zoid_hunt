7490f895a03efc698a8bf6a66dd4ee735e4b9e9a
Dominic Rooney <orangutanrider@gmail.com>
2024-02-19 18:49:12 +0000

    bang progress
    That's the traits mostly sorted out.
    I wanted to add the reference flag trait as a binding, but I've decided not to for now.
    Cause the generics have to be propagated when you do that.
    And it doesn't let met define a trait implementation within a trait.
    
    The next step is macros, and I'll probably handle the reference flag implementation in those.

--- n5_bev_real_mvp1/src/rts_unit/behaviour/bang.rs
+++ n5_bev_real_mvp1/src/rts_unit/behaviour/bang.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use crate::rts_unit::*;
+use crate::rts_unit::{
+    *,
+    behaviour::*,
+};
 
 #[derive(Component)]
 pub struct TBehaviourBang(bool);
@@ -29,18 +32,24 @@ impl TBehaviourBang {
     }
 }
 
-pub trait RefBangToTreeRoot<RefBangExport>
-where RefBangExport: RefBangExport<> {
-    fn to_root_ref_bang(&self, mut root_ref_bang: Mut<RefBangExport>) {
-
+pub trait RefBangToTreeRoot<TreeRoot, Export>
+where TreeRoot: RefBangExport<Export>, Export: BehaviourInterfacedComponent {
+    fn ref_bang_to_root(&self, mut root: Mut<TreeRoot>) {
+        root.ref_bang()
     }
 }
 
 pub trait RefBangExport<Export>
 where Export: BehaviourInterfacedComponent {
+    fn ref_bang(&mut self);
+    fn reset(&mut self);
+    fn bang_val(&self) -> bool;
 
+    fn export(&self, mut export: Mut<Export>){
+        export.set_active(self.bang_val());
+    }
 }
 
 pub trait BehaviourInterfacedComponent: Component {
-
+    fn set_active(&mut self, v: bool);
 }
