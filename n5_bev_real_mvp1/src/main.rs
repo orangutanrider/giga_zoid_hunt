79157906f2395c961e07c082d76f16c79a7086d2
Dominic Rooney <orangutanrider@gmail.com>
2024-02-20 19:23:58 +0000

    main camera
    So next step is to build a unit using the behaviour system, will leave that for tomorrow.

--- n5_bev_real_mvp1/src/main.rs
+++ n5_bev_real_mvp1/src/main.rs
@@ -38,14 +38,13 @@ impl Plugin for InitializePlugin{
             rts_unit::InitializePlugin,
             rts_controller::InitializePlugin,
         ))
-        //.add_systems(Startup, (
-        //    spawn_main_camera,
-        //))
+        .add_systems(Startup, (
+            spawn_main_camera,
+        ))
         ;
     }
 }
 
-/* 
 #[derive(Component)]
 pub struct MainCamera;
 fn spawn_main_camera(mut commands: Commands) {
@@ -53,9 +52,5 @@ fn spawn_main_camera(mut commands: Commands) {
     .spawn((
         MainCamera,
         Camera2dBundle::default(),
-    ))
-    .add(InitMouseTracking)
-    .add(InitWorldTracking)
-    .insert(mouse_tracking::MainCamera);
+    ));
 }
-*/
