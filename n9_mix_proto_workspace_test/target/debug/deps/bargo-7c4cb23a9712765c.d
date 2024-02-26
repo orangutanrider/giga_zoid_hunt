5471e821522522922f4705c512354ed758791071
Dominic Rooney <orangutanrider@gmail.com>
2024-02-26 15:22:59 +0000

    Workspace proto
    I am having trouble setting it up in n8, works here though.
    Will do more testing.

---
+++ n9_mix_proto_workspace_test/target/debug/deps/bargo-7c4cb23a9712765c.d
@@ -0,0 +1,5 @@
+C:\Users\Dominic Rooney\OneDrive\Desktop\active_projects\ART410\repo\GDPAbertayUndergrad-Honours\n9_mix_proto_workspace_test\target\debug\deps\libbargo-7c4cb23a9712765c.rmeta: libs\bargo\src\lib.rs
+
+C:\Users\Dominic Rooney\OneDrive\Desktop\active_projects\ART410\repo\GDPAbertayUndergrad-Honours\n9_mix_proto_workspace_test\target\debug\deps\bargo-7c4cb23a9712765c.d: libs\bargo\src\lib.rs
+
+libs\bargo\src\lib.rs:
---
+++ n9_mix_proto_workspace_test/target/debug/deps/foo-65f7788305b3d0dc.d
@@ -0,0 +1,5 @@
+C:\Users\Dominic Rooney\OneDrive\Desktop\active_projects\ART410\repo\GDPAbertayUndergrad-Honours\n9_mix_proto_workspace_test\target\debug\deps\libfoo-65f7788305b3d0dc.rmeta: bins\foo\src\main.rs
+
+C:\Users\Dominic Rooney\OneDrive\Desktop\active_projects\ART410\repo\GDPAbertayUndergrad-Honours\n9_mix_proto_workspace_test\target\debug\deps\foo-65f7788305b3d0dc.d: bins\foo\src\main.rs
+
+bins\foo\src\main.rs:
