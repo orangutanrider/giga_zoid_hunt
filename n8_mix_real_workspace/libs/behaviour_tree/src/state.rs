0ce7d37c7c57fcdd07e03fdb1e4e4e1e23153a2a
Dominic Rooney <orangutanrider@gmail.com>
2024-02-27 16:52:11 +0000

    Workspace fixed
    Ha...
    I was having issues for around 3 days or more, and it is all because the edition field in the TOML wasn't set.
    Edition, stands for Rust edition; If you don't fill it out it defaults to a 2015 version of Rust.
    (Or it depends or something? In N9 it worked for a while without having to do this.)
    Anyways, that was the root of my problems. Very simple...
    
    But yeah, my issue is that it was asking me to do extern crate, but I knew from observing other people's code, that you don't have to do this, so I kept searching for what was causing it.
    
    It's finally over though, I can get back to doing real work.

---
+++ n8_mix_real_workspace/libs/behaviour_tree/src/state.rs
@@ -0,0 +1,53 @@
+use bevy::prelude::*;
+
+/// A bit mask identifying behaviour state flags.
+#[derive(Component, Reflect, Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[reflect(Component, Hash, PartialEq)]
+#[cfg_attr(feature = "serde-serialize", derive(Serialize, Deserialize))]
+pub struct State(u32);
+
+bitflags::bitflags! {
+    impl State: u32 {
+        const N1 = 1 << 0;
+        const N2 = 1 << 1;
+        const N3 = 1 << 2;
+        const N4 = 1 << 3;
+        const N5 = 1 << 4;
+        const N6 = 1 << 5;
+        const N7 = 1 << 6;
+        const N8 = 1 << 7;
+        const N9 = 1 << 8;
+        const N10 = 1 << 9;
+        const N11 = 1 << 10;
+        const N12 = 1 << 11;
+        const N13 = 1 << 12;
+        const N14 = 1 << 13;
+        const N15 = 1 << 14;
+        const N16 = 1 << 15;
+        const N17 = 1 << 16;
+        const N18 = 1 << 17;
+        const N19 = 1 << 18;
+        const N20 = 1 << 19;
+        const N21 = 1 << 20;
+        const N22 = 1 << 21;
+        const N23 = 1 << 22;
+        const N24 = 1 << 23;
+        const N25 = 1 << 24;
+        const N26 = 1 << 25;
+        const N27 = 1 << 26;
+        const N28 = 1 << 27;
+        const N29 = 1 << 28;
+        const N30 = 1 << 29;
+        const N31 = 1 << 30;
+        const N32 = 1 << 31;
+
+        const ALL = u32::MAX;
+        const NONE = 0;
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::ALL
+    }
+}
