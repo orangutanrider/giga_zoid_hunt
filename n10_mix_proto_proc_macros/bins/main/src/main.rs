0fa7a9040c207d9c28af4411571771ebd9c3af48
Dominic Rooney <orangutanrider@gmail.com>
2024-02-29 13:17:24 +0000

    science

---
+++ n10_mix_proto_proc_macros/bins/main/src/main.rs
@@ -0,0 +1,36 @@
+use proc_macros::*;
+
+fn main() {
+    println!("Hello World");
+
+    //print_as_string!(
+    //    let x = 3;
+    //);
+
+    //turn_into_print!( 
+    //    let x = 10; 
+    //);
+
+    //println!("{}", x);
+
+    //double!(
+    //    println!("el gringo");
+    //);
+
+    // Broken if you do a string, so basically it an inverted print 
+    //turn_into_print!( 
+    //    "let x = 10;"
+    //);
+
+    print_spans!(
+        let x = 12;
+    );
+
+    print_tokens!(
+        let x = 100;
+    );
+
+    print_kind!(
+        let z = 30000;
+    );
+}
