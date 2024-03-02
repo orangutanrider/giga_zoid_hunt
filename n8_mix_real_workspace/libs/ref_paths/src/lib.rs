031cf47de04ae4bc6b78a38445aec181e560bd72
Dominic Rooney <orangutanrider@gmail.com>
2024-03-02 18:06:58 +0000

    New format for pathways
    Yeah so I think I've got the format down and ready here.
    The pseudocode though, I think that is wrong, it confuses me, gonna redo it.

--- n8_mix_real_workspace/libs/ref_paths/src/lib.rs
+++ n8_mix_real_workspace/libs/ref_paths/src/lib.rs
@@ -1,82 +1,73 @@
 use proc_macro::*;
 
-// It'll be like this
-// write_pathway(ident, (ident, "binding"), (ident, "bindi...
-// the first ident is the first entity being passed into the pathway
-// then, the thing in brackets will be repeated any number of times
-// the ident in the brackets represents a field that has declared a query
-// the "binding" is a literal, that represents the binding that is going to be created by the query
-
 #[proc_macro]
 pub fn write_pathway(input: TokenStream) -> TokenStream {
     return TokenStream::new()
 }
 
-// entrance_entity::query1::(optional bindings, ...)::entity_binding::query2::...
-// entrance_entity::{
-//    query1::(optional bindings, ...)::entity_binding::query3::... ,
-//    query2::(optional bindings, ...)::entity_binding::query4::... 
-// }
+// ================================
+// Format
+// "..." continues, restarting the pattern
 
-// Okay, so it can be fully recursive then can't it?
-// restart at every entity I think?, or no?
+// entity::query(,);
 
-// Okay, forget groups for now...
+// entity::query(,) -> entity::query(,) -> ...;
 
-// Algorithm
-// Get entity
-// :: Seperation
-// Get query
-// :: Seperation
-// Enter () group
-// Get optional bindings, if there are any
-// Exit () group
-// if there is a :: Seperation 
-// Restart and expect an entity binding
+// entity::query(,) -> {
+//     entity::query(,) -> ...,
+//     entity::query(,)
+// };
 
-// Hold up though
-// Okay yeah, the entity binding is from the query, it shows which field of the query tuple is expected as an entity
+// entity::{
+//     query(,),
+//     query(,) -> ... 
+// };
 
-// So then expanding to keywords and symbols
-// mut, a query can be defined as mut, if so get_mut is used
-// mut, an optional binding can be defined as mut, if so it is written as a mutable let
-// numbers previous to any binding ident dennote the tuple field being targeted
+// entity1::query1(,);
+// entity1::query2(,) -> {
+//    entity2::query3(,) -> ...,
+//    entity3::query4(,)
+// };
 
-// Okay, and now, expanding to further group declerations
+// ================================
+// Wildcards
+// tuple index (also logic for when there is no tuple index declared, for queries that aren't querying for tuples)
+// query mutability (inferred from binding mutability)
+// binding mutability, binding reference declerations
 
+// ================================
+// Pseudocode
 // 1
-// Get entity
-// :: Seperation
-// Match to either a {group} or a single ident
-
-// 2
+// Get entity 
+// :: 
+// If {group}, move to 2, otherwise continue
 // Get query
-// :: Seperation
 // Enter (group)
 // Get optional bindings, if there are any
 // Exit (group)
-// if there is a :: seperation
-    // Go back to 1 and expect an entity binding
-// if there is a ,
-    // Go back to 2 and expect a query
-
-// Okay...
-// I think that's it
-// I can barely believe it
-
-// Okay no, no, no...
-// It is at the end of optional bindings that the comma check should happen
+// If ; End
+// If -> {group}, move to 3
+// If -> Restart
 
 // 2
 // Get query
-// :: Seperation
 // Enter (group)
 // Get optional bindings, if there are any
 // Exit (group)
-// if there is a ,
-    // Go back to 2 and expect a query
-// if there is a :: seperation
-    // Go back to 1 and expect an entity binding
+// If ,
+    // Restart 2, expect query 
+// If -> 
+    // Go to 3
 
-// Okay yeah no, what I wrote is right
-// jesus man...
+// 3
+// Get entity 
+// :: 
+// If {group}, move to 2, otherwise continue
+// Get query
+// Enter (group)
+// Get optional bindings, if there are any
+// Exit (group)
+// If ,
+    // Restart 3, expect entity 
+// If -> 
+    // Restart 3, expect entity
