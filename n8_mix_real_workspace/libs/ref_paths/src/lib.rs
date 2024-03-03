937a630433c51bcc85136a54cee484748a832bbe
Dominic Rooney <orangutanrider@gmail.com>
2024-03-03 14:07:14 +0000

    pathway macro 1
    So not finished or anything, just thought I'd save this.

--- n8_mix_real_workspace/libs/ref_paths/src/lib.rs
+++ n8_mix_real_workspace/libs/ref_paths/src/lib.rs
@@ -1,9 +1,140 @@
 use proc_macro::*;
 
-#[proc_macro]
-pub fn write_pathway(input: TokenStream) -> TokenStream {
-    return TokenStream::new()
+//#[proc_macro]
+//pub fn write_pathway(input: TokenStream) -> TokenStream {
+//    return TokenStream::new()
+//}
+
+fn entity_step(entity: Option<TokenTree>) {
+    let Some(entity) = entity else {
+        // exit_step();
+        return;
+    };
+    if TokenTree::Ident(entity) != entity {
+        // err_step();
+        return;
+    }
 }
+
+fn entity_query_punct_step(punct: Option<TokenTree>) {
+    let Some(punct) = punct else {
+        // err_step();
+        return;
+    };
+    if TokenTree::Punct(punct) != punct {
+        // err_step();
+        return;
+    }
+
+    // add check for if is :
+}
+
+fn query_step(query: Option<TokenTree>) {
+    let Some(query) = query else {
+        // err_step();
+        return;
+    };
+    if TokenTree::Ident(query) != query {
+        // err_step();
+        return;
+    }
+}
+
+fn binding_group_step(group: Option<TokenTree>) {
+    let Some(group) = group else {
+        // err_step();
+        return;
+    };
+    if TokenTree::Group(group) != group {
+        // err_step();
+        return;
+    }
+
+    // add check for if is ()
+}
+
+fn optional_binding_step(mut group: token_stream::IntoIter) {
+    // add check for mutability decleration
+    // add check for reference symbol decleration
+
+    let binding = group.next();
+    let Some(binding) = binding else {
+        // exit
+        return;
+    };
+    if TokenTree::Ident(binding) != binding {
+        // error
+        return;
+    }
+
+    let comma = group.next();
+    let Some(comma) = comma else {
+        // exit
+        return;  
+    };
+    if TokenTree::Punct(comma) != comma {
+        // error
+        return;
+    }
+
+    // add check for if is ,
+
+    optional_binding_step(group);
+}
+
+fn next_entity_punct_step1(punct: Option<TokenTree>) {
+    let Some(punct) = punct else {
+        // exit
+        return;
+    };
+    if TokenTree::Punct(punct) != punct {
+        // error
+        return;
+    }
+
+    // add check for if is -
+}
+
+fn next_entity_punct_step2(punct: Option<TokenTree>) {
+    let Some(punct) = punct else {
+        // errpr
+        return;
+    };
+    if TokenTree::Punct(punct) != punct {
+        // error
+        return;
+    }
+
+    // add check for if is >
+}
+
+/* 
+fn entity_step(mut iter: token_stream::IntoIter) {
+    // let tuple_index = iter.next();
+
+    
+
+    //let entity = iter.next();
+    //let Some(entity) = entity else {
+    //    //exit_step();
+    //    return;
+    //};
+    //
+    //let punct1 = iter.next();
+    //let Some(punct1) = punct1 else {
+    //    //exit_step();
+    //    return;
+    //};
+    //let punct2 = iter.next();
+    //let Some(punct2) = punct2 else {
+    //    //exit_step();
+    //    return;
+    //};
+
+    let query = iter.next();
+    match
+}
+*/
 
 // ================================
 // Format
