491494f640d0fffc94e501503bad55993c8b2c49
Dominic Rooney <orangutanrider@gmail.com>
2024-03-10 18:50:18 +0000

    n11
    Mostly done now.
    It can still be improved, but I won't go that far for this honours project.
    Still have to hook it up to the macro, but yeah.

--- n11_mix_proto_pathways/src/caravan/query/internal.rs
+++ n11_mix_proto_pathways/src/caravan/query/internal.rs
@@ -7,6 +7,74 @@ pub fn till_query_fin(caravan: Caravan, current: Span,) -> Result<(Caravan, Span
     return join_until_bindings(caravan, current)
 }
 
+/// Will not check for semi-colon
+pub fn query_deep_next(mut caravan: Caravan) -> Result<Caravan, CaravanError> {
+    let token = caravan.next();
+    let Some(token) = token else {
+        return Ok(caravan);
+    };
+
+    // get combined symbols
+    let span = token.span();
+    let token = caravan.next();
+    let Some(token) = token else {
+        return Err(CaravanError::Undefined);
+    };
+    let span = span.join(token.span());
+    let Some(span) = span else {
+        return Err(CaravanError::Undefined)
+    };
+    let span = span.source_text();
+    let Some(span) = span else {
+        return Err(CaravanError::Undefined)
+    };
+
+    // expect '->'
+    if span != "->" {
+        return Err(CaravanError::Undefined);
+    }
+
+    return entity_step(caravan)
+}
+
+pub fn query_surface_next(mut caravan: Caravan) -> Result<Caravan, CaravanError> {
+    let token = caravan.next();
+    let Some(token) = token else {
+        return Ok(caravan);
+    };
+
+    let symbol = token.to_string();
+    if symbol == ";" {
+        return entity_step(caravan)
+    }
+
+    if symbol != "-" {
+        return Err(CaravanError::Undefined)
+    }
+
+    // get combined symbols
+    let span = token.span();
+    let token = caravan.next();
+    let Some(token) = token else {
+        return Err(CaravanError::Undefined);
+    };
+    let span = span.join(token.span());
+    let Some(span) = span else {
+        return Err(CaravanError::Undefined)
+    };
+    let span = span.source_text();
+    let Some(span) = span else {
+        return Err(CaravanError::Undefined)
+    };
+
+    // expect '->'
+    if span != "->" {
+        return Err(CaravanError::Undefined);
+    }
+
+    return entity_step(caravan)
+}
+
 fn join_until_bindings(mut caravan: Caravan, current: Span) -> Result<(Caravan, Span, Group), CaravanError> {
     let token = caravan.next();
     let Some(token) = token else {
