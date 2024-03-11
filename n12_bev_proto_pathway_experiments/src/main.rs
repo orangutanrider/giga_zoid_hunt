9f3b5c55b55b8835c5c029fd54a9b44c85ff6950
Dominic Rooney <orangutanrider@gmail.com>
2024-03-11 11:53:49 +0000

    n12 test1
    Yeah, the values can be expected in the first let, resulting from the query get.

--- n12_bev_proto_pathway_experiments/src/main.rs
+++ n12_bev_proto_pathway_experiments/src/main.rs
@@ -1,5 +1,21 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+/// OriginToMid
+/// MidToDestination
+/// Destination
+struct Test1Flag(Entity);
+
+#[derive(Component)]
+struct OriginToMid(Entity);
+
+#[derive(Component)]
+struct MidToDestination(Entity);
+
+#[derive(Component)]
+struct Destination(bool);
+
+
 fn main() {
     println!("Hello, bevy.");
 
@@ -7,3 +23,21 @@ fn main() {
 
     app.run();
 }
+
+// Test to see if the error can be expected in the first let statement, from the query get
+fn test1(
+    q_origin: Query<&OriginToMid, With<Test1Flag>>,
+    q_mid: Query<&MidToDestination>,
+    mut q_destination: Query<&mut Destination>,
+) {
+    for origin in q_origin.iter() {
+        let Ok(mid) = q_mid.get(origin.0) else {
+            return;
+        };
+        let Ok(mut destination) = q_destination.get_mut(mid.0) else {
+            return;
+        };
+
+        destination.0 = true;
+    }
+} // Yeah, this seems to work
