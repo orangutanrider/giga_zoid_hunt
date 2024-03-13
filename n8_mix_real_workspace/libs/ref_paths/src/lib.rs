b63ddd334f6ebe87e8c5b712bd913559d8ff20fc
Dominic Rooney <orangutanrider@gmail.com>
2024-03-13 13:20:12 +0000

    ref pathways done

--- n8_mix_real_workspace/libs/ref_paths/src/lib.rs
+++ n8_mix_real_workspace/libs/ref_paths/src/lib.rs
@@ -3,16 +3,37 @@ mod edit;
 
 use bevy::prelude::*;
 
+// A reference flag is simply a classification of a component's purpose
+// It requires no implementation
+
+// A self entity reference or "Cairn" (W.I.P. naming)
+// Is simply a classifcation of a component's purpose
+// It requires no implementation
+
 /// An internal (internal to an entity heirarchy) entity reference.
 pub trait Waymark {
     fn go(&self) -> Entity;
 }
 
+#[macro_export]
 macro_rules! waymark {
     ($t: ty) => {
         impl Waymark for $t {
             fn go(&self) -> Entity {
                 return self.0
+            }
+        }
+
+        impl Default for $t {
+            fn default() -> Self {
+                return Self(Entity::PLACEHOLDER)
+            }
+        }
+
+        impl $t {
+            /// dest as in destination
+            fn new(dest: Entity) -> Self {
+                return Self(dest)
             }
         }
     };
