c0ded1303df9683986bf48038edb33bad4251036
Dominic Rooney <orangutanrider@gmail.com>
2024-03-17 13:35:25 +0000

    behaviour tree, state stuff

--- n8_mix_real_workspace/libs/behaviour_tree/src/lib.rs
+++ n8_mix_real_workspace/libs/behaviour_tree/src/lib.rs
@@ -1,1 +1,12 @@
 pub mod state;
+
+use bevy::prelude::*;
+use ref_paths::*;
+
+#[derive(Component)]
+pub struct ToParentNode(Entity);
+waymark!(ToParentNode);
+
+#[derive(Component)]
+pub struct ToBehaviourRoot(Entity);
+waymark!(ToBehaviourRoot);
