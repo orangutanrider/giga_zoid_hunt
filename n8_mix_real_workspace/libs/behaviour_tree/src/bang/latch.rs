a5d7d91ede86da37cfbefa2c1b487dc3af19005a
Dominic Rooney <orangutanrider@gmail.com>
2024-03-18 15:02:28 +0000

    latch rework
    It's still the same system, but I've changed the 'blueprints' to not use a trait.
    A trait ties the logic to the definition of a component, but the logic should be separate, so I've done that.
    Now, there are simply generic functions, that act like the blueprints.
    They are prefabs, in the sense that they automate the most common format that these latch systems will take.
    If a dev wanted, they could make their own custom latch system, not using this stuff here.
    Showing the fact, that these generic functions, are just for automation.

--- n8_mix_real_workspace/libs/behaviour_tree/src/bang/latch.rs
+++ n8_mix_real_workspace/libs/behaviour_tree/src/bang/latch.rs
@@ -6,43 +6,84 @@ use bevy::{
 use super::*;
 use crate::{state::terminal::TState, ToParentNode};
 
-/// A bang latch is a classification of component.
-/// It will flag system(s) that can activate the local bang terminal.
-/// The trait provides a blueprint for doing so.
-/// (Via reading the parent node's state, and deciding, using its own written logic.)
-/// (Latches should not attempt activation, when the parent node is not active.)
-/// (Latches only need to update, when their parent node has changed.)
-pub(crate) trait BangLatch {
-    fn set_bang(
-        mut bang: Mut<TBang>,
-        parent_state: &TState,
-        parent_bang: &TBang,
-        propogator: &LatchPropagator,
-    ) {
-        // (Latches should not attempt activation, when the parent node is not active.)
-        // (Latches only need to update, when their parent node has changed.)
-        if !parent_bang.active() || !propogator.propagating() {
-            return;
-        }
-
-        bang.set_bang(Self::latch_logic(parent_state, parent_bang));
-    }
-
-    fn latch_logic(
-        parent_state: &TState,
-        parent_bang: &TBang,
-    ) -> bool; // (Via reading the parent node's state, and deciding, using its own written logic.)
-}
+// A bang latch is a classification of component.
+// It will flag system(s) that can activate the local bang terminal.
+// The systems are expected to do this via reading the parent node's state, and deciding using their own logic.
+// They are not expected to switch on their latch, if the parent's bang is inactive.
+// They only need to run when their parent node changes, which is inferred through the latch propagator component.
 
 #[derive(SystemParam)]
 /// Standard query set for bang latch systems
-pub struct LatchQueries<'w, 's, Latch: Component> {
-    node_q: Query<'w, 's, (&'static TBang, &'static LatchPropagator, &'static ToParentNode), (With<Latch>, Changed<LatchPropagator>)>,
-    parent_q:  Query<'w, 's, &'static mut TState>,
+pub(crate) struct LatchQueries<'w, 's, Latch: Component> {
+    pub node_q: Query<'w, 's, (&'static mut TBang, &'static LatchPropagator, &'static ToParentNode), (With<Latch>, Changed<LatchPropagator>)>,
+    pub parent_q:  Query<'w, 's, (&'static TState, &'static TBang)>,
+}
+
+/// Prefab system for bang latches that are flagged by a single component
+pub(crate) fn bang_latch_sys<F, Latch: Component>(
+    mut latch_qs: LatchQueries<Latch>,
+    latch_logic: F
+) where F: Fn(&TState, &TBang) -> bool { 
+    let mut node_q = latch_qs.node_q;
+    let parent_q = &latch_qs.parent_q;
+
+    for (local_bang, propagator, to_parent) in node_q.iter_mut() {
+        latch_set_bang(local_bang, propagator, to_parent, parent_q, &latch_logic)
+    }
+}
+
+/// Prefab function for bang latch systems
+pub(crate) fn latch_set_bang<F>(
+    mut local_bang: Mut<TBang>,
+    propagator: &LatchPropagator,
+    to_parent: &ToParentNode,
+    parent_q: &Query<(&TState, &TBang)>,
+    latch_logic: F
+) where F: Fn(&TState, &TBang) -> bool { 
+    ref_caravan!(to_parent::parent_q((parent_state, parent_bang)););
+    
+    // (Latches should not attempt activation, when the parent node is not active.)
+    // (Latches only need to update, when their parent node has changed.)
+    if !parent_bang.active() || !propagator.propagating() {
+        return;
+    }
+
+    local_bang.set_bang(latch_logic(parent_state, parent_bang));
 }
 
 #[derive(Component)]
-/// When a bang terminal is changed to true, the child latch propagators get activated, by a system.
+/// A basic latch will activate its node, if the parent's bang is active.
+/// It does not care about the parent's state.
+pub struct BasicLatch;
+
+fn basic_latch_sys(
+    mut node_q: Query<(&mut TBang, &LatchPropagator, &ToParentNode), (With<BasicLatch>, Changed<LatchPropagator>)>,
+    parent_q: Query<&TBang>,
+) {
+    for (local_bang, propagator, to_parent) in node_q.iter_mut() {
+        basic_latch_set_bang(local_bang, propagator, to_parent, &parent_q)
+    }
+}
+
+fn basic_latch_set_bang(
+    mut local_bang: Mut<TBang>,
+    propagator: &LatchPropagator,
+    to_parent: &ToParentNode,
+    parent_q: &Query<&TBang>,
+) {
+    ref_caravan!(to_parent::parent_q(parent_bang););
+
+    // (Latches should not attempt activation, when the parent node is not active.)
+    // (Latches only need to update, when their parent node has changed.)
+    if !parent_bang.active() || !propagator.propagating() {
+        return;
+    }
+
+    local_bang.set_bang(true);
+}
+
+#[derive(Component)]
+/// When a bang terminal is changed to true, the  latch propagators, on the children of that entity, get activated, by a system.
 /// When a latch propagator is activated, the latch systems can check to see if their bang terminal should be activated now.
 /// (And activate it, if it should.) (Restarting the cycle of propagation.)
 pub(crate) struct LatchPropagator(bool);
@@ -61,7 +102,7 @@ impl LatchPropagator {
     }
 }
 
-fn latch_propogation_sys(
+fn latch_propagation_sys(
     mut node_q: Query<(&TBang, &Children, &mut LatchPropagator), Changed<TBang>>,
     mut child_q: Query<&mut LatchPropagator>
 ) {
