891cf085460d47aab4277ed9a23b2103c9cb5a54
Dominic Rooney <orangutanrider@gmail.com>
2024-04-03 11:53:00 +0100

    unit nav
    need detectors to finish

---
+++ n8_mix_real_workspace/libs/rts_unit_nav/src/follow_attack_move.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+use rts_unit_control::prelude::*;
+
+use crate::*;
+
+#[derive(Component)]
+pub struct AttackMoveNavigation;
+
+pub fn attack_move_navigation_system(
+
+) {
+
+}
